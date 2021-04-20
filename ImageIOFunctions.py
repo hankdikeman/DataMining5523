cb12b46098236814b26fb2d89829f071014356f1
hankdikeman <dikem003@umn.edu>
2021-04-20 13:11:17 -0500

    outline for data classification

---
+++ ImageIOFunctions.py
@@ -0,0 +1,18 @@
+"""
+This file contains some simple IO functions to read and write image data from CSV
+Author: Henry Dikeman
+Email:  dikem003@umn.edu
+Date:   04/20/21
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+# helper function to load label data from CSV and trim header and label column
+def LoadImageFromCSV(fname):
+    # load filename using numpy builtin
+    rawlabels = np.genfromtxt(fname, delimiter=',')
+    # trim header and column label
+    labels = rawlabels[1:,1:]
+    # return trimmed label data array
+    return labels
--- initial_visualization.py
+++ initial_visualization.py
@@ -4,3 +4,25 @@ Author: Henry Dikeman
 Email:  dikem003@umn.edu
 Date:   04/20/21
 """
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import os
+from ImageIOFunctions import LoadImageFromCSV
+
+# tests for visualization of label data
+if __name__ == "__main__":
+    # store CL argument to determine which image to show
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--filename', action='store', dest='filename', help='Filename of file to be visualized')
+    args = parser.parse_args()
+
+    # generate filename
+    fname = os.path.join(os.getcwd(),args.filename)
+    # load data to numpy array with helper function
+    labels = LoadImageFromCSV(fname)
+
+    # display labeled image with imshow
+    plt.imshow(labels, cmap='gray', vmin=0, vmax=1)
+    plt.show()
+
